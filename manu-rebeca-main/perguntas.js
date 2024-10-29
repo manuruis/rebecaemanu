bdc51e18c8bcfe327766efb9f8a77282af6214e5
manuruis <132909727+manuruis@users.noreply.github.com>
2024-10-29 09:46:32 -0300

    Add files via upload

---
+++ manu-rebeca-main/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quizz de Princesas' ,
+    'Qual a primeira princesa da Disney?',
+    'Branca de Neve'
+)
+
+criaCartao(
+    'Quizz de Princesas',
+    'Qual a nova princesa da Disney?',
+    'Asha'
+)
+
+criaCartao(
+    '\quizz de princesa',
+    'Qual o filme de princesa mais caro da Disney',
+    'Bela Adormecida'
+)
+
+criaCartao(
+    'Quizz de Princesas',
+    'Qual a princesa mais famosa da Disney',
+    'Ariel'
+)
